f009727e816a244f9ecdcb4b847b2baaa09ae7e9
Mariano De Simone <desimone.mariano@gmail.com>
2024-11-19 22:48:54 +0100

    Fix Api Route

--- app/api/drugs/[id]/route.ts
+++ app/api/drugs/[id]/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 import drugsData from "@/app/api/data/mock-drugs.json";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   const drug = drugsData.find((drug) => drug.id === id);
